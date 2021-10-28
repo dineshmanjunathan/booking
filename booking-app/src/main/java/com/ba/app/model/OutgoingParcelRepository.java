e3a5455dc8299acc847ac0e5d7cd7ba51e891945
Vinoth <dineshmanjunathan@gmail.com>
2021-10-28 14:33:01 +0530

    Committed for incoming parcel screen

--- booking-app/src/main/java/com/ba/app/model/OutgoingParcelRepository.java
+++ booking-app/src/main/java/com/ba/app/model/OutgoingParcelRepository.java
@@ -2,15 +2,21 @@ package com.ba.app.model;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.ba.app.entity.OutgoingParcel;
 
 public interface OutgoingParcelRepository extends CrudRepository<OutgoingParcel, String> {
 	
-	List<OutgoingParcel> findByFromLocationAndToLocationAndBookedOn(String fromLocation,String toLocation,String bookedOn);
-	List<OutgoingParcel> findByFromLocationAndToLocation(String fromLocation,String toLocation);
 
 	OutgoingParcel findByOgplNo(long ogplNo);
+	
+	@Query(value="select * from t_outgoing_parcel where ogpl_no not in (select ogpl_no from t_inventory ti2) and to_location =?2 and from_location =?1 and booked_on =?3",nativeQuery=true)
 
+	List<OutgoingParcel> findByFromLocationAndToLocationAndBookedOn(String fromLocation,String toLocation,String bookedOn);
+	
+	@Query(value="select * from t_outgoing_parcel where ogpl_no not in (select ogpl_no from t_inventory ti2) and to_location =?2 and from_location =?1",nativeQuery=true)
+
+	List<OutgoingParcel> findByFromLocationAndToLocation(String fromLocation,String toLocation);
 }
